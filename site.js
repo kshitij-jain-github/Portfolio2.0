3cdc46da4d41ac3f174bc8f5b78335c140d0e0aa
Kshitij Jain <jainkshitij1530@gmail.com>
2024-01-27 12:33:38 +0530

    changes

--- site.js
+++ site.js
@@ -224,12 +224,28 @@
      pointerY = null;
    
    }
+      
+
+// image slider
    
 
+   const slider = document.querySelector('.slider');
+
+   function activate(e) {
+     const items = document.querySelectorAll('.item');
+     e.target.matches('.next') && slider.append(items[0])
+     e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+   }
+   
+   document.addEventListener('click',activate,false);
 
 
 
- 
+
+
+
+
+   
 // var myVar;
 
 // function myFunction() {
